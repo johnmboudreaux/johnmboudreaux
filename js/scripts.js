751e58b77f10455a73faaa2828ae858a60a5643b
johnmboudreaux <jhnbdrx@gmail.com>
2018-09-14 18:54:23 -0600

    changing things up in header. adding a hamburger menu and links

--- js/scripts.js
+++ js/scripts.js
@@ -15,4 +15,29 @@ $(function() {
     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 'linear');
   });
 
+  $('.menu-btn').on('click', toggleMenu);
+
+ function toggleMenu() {
+   console.log('tm');
+   
+   let menuShowing;
+  //  console.log(menuShowing);
+  debugger
+    if (!menuShowing) {
+      $('.menu-btn').addClass('close');
+      $('.menu').css("opacity", "0.9");
+
+      console.log('menushowung1', menuShowing);
+      menuShowing = true;
+      console.log('menushowung1', menuShowing);
+      
+    } else if(menuShowing) {
+      $('.menu-btn').removeClass('close');
+      $('.menu').css("opacity", "0");
+
+      menuShowing = false;
+      console.log("menushowung2", menuShowing);
+    }
+  }
+
 });
